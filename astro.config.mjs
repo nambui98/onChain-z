05fdfc5611d2492ac2c68b1d311178debae6766a
Thach Lockevn <lockevn@gmail.com>
2022-08-14 22:53:07 +0700

    buildable on Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,13 @@
 
 import { defineConfig } from "astro/config"
 import preact from "@astrojs/preact"
+import vercel from "@astrojs/vercel/serverless"
 
 // https://astro.build/config
 
 export default defineConfig({
   integrations: [preact()],
+
+  output: "server",
+  adapter: vercel(),
 })
