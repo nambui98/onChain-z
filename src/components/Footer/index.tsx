659d71370d65d553da932a5d719ff16909ab297c
Thach Lockevn <lockevn@gmail.com>
2022-08-14 23:38:20 +0700

    v0.0.2

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,11 +1,13 @@
 import { h } from "preact"
 import Styles from "./styles.module.scss"
-
+import packagejson from "../../../package.json"
 function Footer() {
   return (
     <footer className={Styles.footer}>
       &copy; {new Date().getFullYear()} GurucoreAuthor
-      <small className={Styles.byline}>🚀 Gurucore.com Astro Tina</small>
+      <small className={Styles.byline}>
+        🚀 Gurucore.com {packagejson.productName} v{packagejson.version}
+      </small>
     </footer>
   )
 }
