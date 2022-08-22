c3d45e3da30e4d5ed8b16972a37647a6dd774fd9
Thach Lockevn <lockevn@gmail.com>
2022-08-22 16:38:28 +0700

    ArticleHero

---
+++ src/components/Article/ArticleHero.tsx
@@ -0,0 +1,12 @@
+import styles from './ArticleHero.module.scss'
+
+export default function ArticleHero({ content, children = null }) {
+  const { image, title } = content || {}
+  return (
+    <>
+      <header style={`background-image:url(${image})`} class={styles.hero}>
+        {children}
+      </header>
+    </>
+  )
+}
