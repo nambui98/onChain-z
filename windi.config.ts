e3e3a2905c18be54dce2d7afa5755f201ef8bbad
Thach Lockevn <lockevn@gmail.com>
2022-09-05 18:28:22 +0700

    some styles

--- windi.config.ts
+++ windi.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'windicss/helpers'
 import formsPlugin from 'windicss/plugin/forms'
 import typographyPlugin from 'windicss/plugin/typography'
+import colors from 'windicss/colors'
 
 export default defineConfig({
   darkMode: false, // or 'media' or 'class'
@@ -10,10 +11,28 @@ export default defineConfig({
     container: false,
   },
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: colors.sky,
+        red: colors.rose,
+        pink: colors.fuchsia,
+      },
+      fontFamily: {
+        sans: ['Segoe UI', 'sans-serif'],
+        serif: ['Cambria', 'serif'],
+      },
+    },
   },
+  shortcuts: {
+    btn: 'py-2 px-4 font-semibold rounded-lg shadow-md',
+
+    heading2: 'font-sans font-semibold text-3xl text-left uppercase align-middle break-all',
+    title: 'font-sans text-2xl text-left uppercase align-middle break-all',
+    'reading-content': 'leading-relaxed font-sans break-all',
+  },
+
   extract: {
-    include: ['./src/**/*.{vue,html,jsx,tsx,astro}'],
+    include: ['./src/**/*.{vue,html,jsx,tsx,astro,mdx,pug}'],
     exclude: ['node_modules', '.git'],
   },
   plugins: [typographyPlugin, formsPlugin],
