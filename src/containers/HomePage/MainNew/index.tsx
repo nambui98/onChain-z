244968823a304d8e2c2d810c42a4054cd09f4dc3
NamBui98 <bvnam98@gmail.com>
2022-09-17 19:22:09 +0700

    fix deploy

--- src/containers/HomePage/MainNew/index.tsx
+++ src/containers/HomePage/MainNew/index.tsx
@@ -66,8 +66,6 @@ export default function MainNew({ }: Props) {
                     }
                 })
             } */}
-
-
             <div className="w-448px bg-no-repeat bg-cover relative rounded-2xl" style={{
                 backgroundImage: `url(https://s3-alpha-sig.figma.com/img/51ad/f561/3c6f7e0da8c01af4fd95da68ca862f89?Expires=1663545600&Signature=CpB6kZIgY9NAgpHrpXfz5mtndvEEcV-yG-yoiYIvk9K-UKlLiIYYjCW7fHgxQBoJmIemjml-lXpV0elGXhUS5KkUjZWiv4BrSDPphjf3TZkTmk2rAaz0ilAcax6NjzfkYXKpVaimn7n-W66Saxp2juOBaIhXgaRk5t~PwLy1YGjje7yf-R54RqRIJVZudID6uv1zVmIn7gCAVPXt0-HfxHfk~niotxtLTuVPW-3ThIg3WjCCDMxwWMKFbVZXkRQ5d3wZfImPHnAIXYXF1FJVj8lpJCwhoVbZRlBvrxX7N1Eo70nW78h0wch007HtH3lVX6UZc~pqFiCHZie8CvTDiQ__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA)`,
             }}>
