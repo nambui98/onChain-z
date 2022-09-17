95f6c9cab3d8a1900c80b850feec01e098a6a66a
Thach Lockevn <lockevn@gmail.com>
2022-09-17 13:13:45 +0700

    fix error while importing SVG file

--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -1,16 +1,14 @@
-import rectangle from '/assets/icons/rectangle.svg'
-
 type Props = {
-    name: String
+  name: String
 }
 
 export default function Title({ name }: Props) {
-    return (
-        <div className="flex items-baseline">
-            <div>
-                <img src={rectangle} className="align-baseline" alt="" />
-            </div>
-            <h5 className="text-2xl font-bold text-secondary ml-8px">{name}</h5>
-        </div>
-    )
+  return (
+    <div className="flex items-baseline">
+      <div>
+        <img src="/assets/icons/rect.svg" className="align-baseline" alt="" />
+      </div>
+      <h5 className="text-2xl font-bold text-secondary ml-8px">{name}</h5>
+    </div>
+  )
 }
