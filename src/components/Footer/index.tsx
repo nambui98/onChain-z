2ad6f88b00b12c478108f800ceaca24393ebbc7f
Thach Lockevn <lockevn@gmail.com>
2022-09-19 10:40:55 +0700

    misc UI

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,11 +1,14 @@
 import AppConfig from '~/AppConfig'
+import * as packagejson from '../../../package.json'
 import { CommonProp } from '~/types/ArticleComponentProps.type'
 
+const ShortLink = () => {
+  return <p className="text-xs font-semibold text-grey80 leading-20px mt-4">Short link no 001</p>
+}
 function Footer(props: CommonProp) {
   return (
     <footer className={'AppFooter ' + props.className ?? ''}>
       <div className="md:container max-w-1120px md:mx-auto flex justify-between">
-
         <div className="w-3/8">
           <a href="/" className="flex items-center text-28px font-bold text-grey">
             <img src="/assets/images/logo.svg" className="mr-4px" alt="" />
@@ -15,29 +18,32 @@ function Footer(props: CommonProp) {
         </div>
         <div>
           <p className="text-sm font-semibold text-grey leading-22px">QUICK LINK 1</p>
-          <p className="text-xs font-semibold text-grey80 leading-20px mt-4">Short link no 001</p>
-          <p className="text-xs font-semibold text-grey80 leading-20px mt-4">Short link no 001</p>
-          <p className="text-xs font-semibold text-grey80 leading-20px mt-4">Short link no 001</p>
-          <p className="text-xs font-semibold text-grey80 leading-20px mt-4">Short link no 001</p>
+          {Array(4)
+            .fill(0)
+            .map((r) => (
+              <ShortLink />
+            ))}
         </div>
         <div>
           <p className="text-sm font-semibold text-grey leading-22px">QUICK LINK 1</p>
-          <p className="text-xs font-semibold text-grey80 leading-20px mt-4">Short link no 001</p>
-          <p className="text-xs font-semibold text-grey80 leading-20px mt-4">Short link no 001</p>
-          <p className="text-xs font-semibold text-grey80 leading-20px mt-4">Short link no 001</p>
-          <p className="text-xs font-semibold text-grey80 leading-20px mt-4">Short link no 001</p>
+          {Array(3)
+            .fill(0)
+            .map((r) => (
+              <ShortLink />
+            ))}
         </div>
         <div>
           <p className="text-sm font-semibold text-grey leading-22px">QUICK LINK 1</p>
-          <p className="text-xs font-semibold text-grey80 leading-20px mt-4">Short link no 001</p>
-          <p className="text-xs font-semibold text-grey80 leading-20px mt-4">Short link no 001</p>
-          <p className="text-xs font-semibold text-grey80 leading-20px mt-4">Short link no 001</p>
-          <p className="text-xs font-semibold text-grey80 leading-20px mt-4">Short link no 001</p>
+          {Array(3)
+            .fill(0)
+            .map((r) => (
+              <ShortLink />
+            ))}
         </div>
       </div>
       <div className="border-1 p-6 mt-10">
-        <div className="md:container max-w-1120px md:mx-auto text-grey text-sm font-semibold">
-          {AppConfig.Copyright}
+        <div className="md:container max-w-1120px md:mx-auto text-grey text-sm font-semibold" title={`🚀 ${packagejson.releasenote} ${packagejson.productName} v${packagejson.version}`}>
+          {AppConfig.Copyright} {AppConfig.SiteName}
         </div>
       </div>
     </footer>
--- src/containers/HomePage/LatestNews/index.tsx
+++ src/containers/HomePage/LatestNews/index.tsx
@@ -4,6 +4,22 @@ import { Heading2 } from '~/components/Heading'
 import { ArticleListProp } from '~/types/ArticleComponentProps.type'
 import './style.scss'
 
+const Separator = () => {
+  return <hr className="border-primary bg-primary  border-2 my-6"></hr>
+}
+
+const CardRelatedList = () => {
+  return (
+    <ul className="">
+      {Array(3)
+        .fill(0)
+        .map((c, i) => {
+          return <li className="text-grey80 text-sm cursor-pointer font-semibold">{`Finblox ${i} ra mắt “Pool Party”, rút thăm may mắn hàng tuần không mất phí`}</li>
+        })}
+    </ul>
+  )
+}
+
 export default function LatestNews({ articles, className }: ArticleListProp) {
   return (
     <div className={`LatestNews ${className ?? ''}`}>
@@ -11,30 +27,18 @@ export default function LatestNews({ articles, className }: ArticleListProp) {
       <div className="flex gap-x-8 mt-6">
         <div className="w-1/3">
           <Card article={articles[0]} isNotShowAuthor={true} />
-          <hr className="border-primary bg-primary  border-2 my-6"></hr>
-          <ul className="">
-            <li className="text-grey80 text-sm cursor-pointer font-semibold">Finblox ra mắt “Pool Party”, rút thăm may mắn hàng tuần không mất phí</li>
-            <li className="text-grey80 text-sm cursor-pointer font-semibold">Finblox ra mắt “Pool Party”, rút thăm may mắn hàng tuần không mất phí</li>
-            <li className="text-grey80 text-sm cursor-pointer font-semibold">Finblox ra mắt “Pool Party”, rút thăm may mắn hàng tuần không mất phí</li>
-          </ul>
+          <Separator />
+          <CardRelatedList />
         </div>
         <div className="w-1/3">
           <Card article={articles[0]} isNotShowAuthor={true} />
-          <hr className="border-primary bg-primary  border-2 my-6"></hr>
-          <ul className="">
-            <li className="text-grey80 text-sm cursor-pointer font-semibold">Finblox ra mắt “Pool Party”, rút thăm may mắn hàng tuần không mất phí</li>
-            <li className="text-grey80 text-sm cursor-pointer font-semibold">Finblox ra mắt “Pool Party”, rút thăm may mắn hàng tuần không mất phí</li>
-            <li className="text-grey80 text-sm cursor-pointer font-semibold">Finblox ra mắt “Pool Party”, rút thăm may mắn hàng tuần không mất phí</li>
-          </ul>
+          <Separator />
+          <CardRelatedList />
         </div>
         <div className="w-1/3">
           <Card article={articles[0]} isNotShowAuthor={true} />
-          <hr className="border-primary bg-primary border-2 my-6"></hr>
-          <ul className="text-grey80 text-sm font-semibold">
-            <li className="transition duration-300 ease-out cursor-pointer  hover:text-primary">Finblox ra mắt “Pool Party”, rút thăm may mắn hàng tuần không mất phí</li>
-            <li className="transition duration-300 ease-out cursor-pointer  hover:text-primary">Finblox ra mắt “Pool Party”, rút thăm may mắn hàng tuần không mất phí</li>
-            <li className="transition duration-300 ease-out cursor-pointer  hover:text-primary">Finblox ra mắt “Pool Party”, rút thăm may mắn hàng tuần không mất phí</li>
-          </ul>
+          <Separator />
+          <CardRelatedList />
         </div>
       </div>
     </div>
