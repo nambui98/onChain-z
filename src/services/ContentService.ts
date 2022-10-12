f96b037eb7d54d1ec7856c1a5fff5778344141d4
Thach Lockevn <lockevn@gmail.com>
2022-10-12 19:16:55 +0700

    v0.9.3 better ContentService

--- src/services/ContentService.ts
+++ src/services/ContentService.ts
@@ -1,17 +1,26 @@
 import { getRandomItems, itemArticles } from './mockdata'
+import { flatten, uniq, concat, uniqBy, clone } from 'lodash'
 
 export interface Author {
-  id: string
+  title: string
   name: string
   image: string
+  summary?: string
+  tags?: string[]
+  body: string
 }
 
 export interface Post {
   id: string
+  uuid: string
   url: string
   title: string
+  body: string
+  draft: false
   image: string
+  tags: string[]
   order?: number
+  category: string
   author?: Author
   publishedDate: string
   description: string
@@ -21,6 +30,8 @@ export class ContentService {
   protected store: any = {}
 
   constructor() {
+    const self = this
+
     // https://vitejs.dev/guide/features.html#glob-import
     // https://discordapp.com/channels/830184174198718474/1020969761087168512
     let articles: any[] = import.meta.glob('/src/pages/articles/**/*.md', { eager: true })
@@ -35,13 +46,36 @@ export class ContentService {
     categories = this.linkArticles(categories, articles)
 
     authors = Object.values(authors).map(this.convertAuthor_NetlifyCMS_Lakdak)
+
     authors = this.linkArticlesToAuthor(authors, articles)
     // TODO: loop articles, decorate author info, relatedArticles (relation, same tag)
+    // decorate author field with authorDTO
+    articles.map((a) => {
+      const authorDto = self.getAuthor(a.author, authors)
 
-    // TODO: loop articles, relatedArticles (relation, same tag)
+      if (authorDto) {
+        // shalow clone, to avoid cyclic references
+        a.author = {
+          url: authorDto.url,
+          title: authorDto.title,
+          image: authorDto.image,
+          summary: authorDto.summary,
+          tags: authorDto.tags,
+          body: authorDto.body,
+        }
+      }
 
-    // TODO: pivot to transform to tags
-    let tags = []
+      return a
+    })
+
+    // pivot to transform to tags
+    let tags = {}
+    const allAvailableTags = uniq(flatten(articles.map((a) => a.tags)))
+    for (const tagName of allAvailableTags) {
+      tags[tagName] = {
+        articles: articles.filter((a) => a.tags.indexOf(tagName) >= 0),
+      }
+    }
 
     this.store = {
       categories,
@@ -49,14 +83,6 @@ export class ContentService {
       authors,
       tags,
     }
-
-    // const posts = RawRepo.articles
-    //   .sort((a, b) => {
-    //     const aDate = new Date(b.frontmatter.publishDate)
-    //     const bDate = new Date(a.frontmatter.publishDate)
-    //     return aDate.getTime() - bDate.getTime()
-    //   })
-    //   .slice(0, 5)
   }
 
   /** convert NetlifyCMS Article frontmatter format to our Lakdak Article plain object format */
@@ -86,8 +112,36 @@ export class ContentService {
     return authors
       ?.filter(({ title }) => !!title)
       ?.map((author) => {
-        return { ...author, articles: articles.filter((r) => r.author === author.title) }
+        return { ...author, articles: clone(articles.filter((r) => (r.author || r.author?.title) === author.title)) }
       })
+  }
+
+  /**
+   *
+   * @param articles array of articles to sort
+   * @param maxCount max number of items in returning result
+   * @returns
+   */
+  protected sortArticlesByPublishDate(articles, maxCount?) {
+    let ret = articles.sort((a, b) => {
+      const aDate = new Date(b.publishDate)
+      const bDate = new Date(a.publishDate)
+      return aDate.getTime() - bDate.getTime()
+    })
+
+    if (maxCount > 0) {
+      ret = ret.slice(0, maxCount)
+    }
+
+    return ret
+  }
+
+  getArticle(urlOrUuid) {
+    return (
+      this.store.articles
+        //
+        .filter((a) => a.url == urlOrUuid || a.uuid == urlOrUuid)[0]
+    )
   }
 
   getArticles() {
@@ -95,10 +149,11 @@ export class ContentService {
   }
 
   /** only return published item, order by publishDate */
-  getPublishedArticles() {
-    return this.getArticles()
-      .filter(({ publishDate }) => !!publishDate)
-      .sort((a, b) => new Date(b.publishDate).valueOf() - new Date(a.publishDate).valueOf())
+  getPublishedArticles(articles) {
+    let ret = (articles || this.getArticles()).filter(({ publishDate }) => !!publishDate)
+
+    ret = this.sortArticlesByPublishDate(ret)
+    return uniqBy(ret, 'uuid')
 
     // const unresolved = posts.map(async (post) => {
     //   const slug = post.frontmatter.slug || post.file.split('/')[post.file.split('/').length - 2]
@@ -113,6 +168,18 @@ export class ContentService {
     // })
 
     // return await Promise.all(unresolved)
+  }
+
+  /** loop articles, return relatedArticles by using same tags */
+  getRelateArticles(article) {
+    if (!article || !article.tags) return []
+
+    let ret = []
+    for (const tagName of article.tags) {
+      ret = concat(ret, this.store.tags[tagName].articles)
+    }
+
+    return this.getPublishedArticles(ret)
   }
 
   getArticles_HomeHighlightItems() {
@@ -130,7 +197,7 @@ export class ContentService {
     return this.store.categories
   }
 
-  getAuthor(id) {
-    return this.store.authors.filter((a) => a.uuid === id)[0]
+  getAuthor(id, authors?) {
+    return (authors || this.store.authors)?.filter((a) => a.title === id)[0]
   }
 }
